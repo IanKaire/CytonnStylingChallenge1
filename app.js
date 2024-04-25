0c0d837dc54b666e3dcab8ab04f81cf57d28c47a
Ian Kaire <ikaire@CYTONNs-MacBook-Pro-5.local>
2024-04-25 17:00:20 +0300

    updated project

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ const searchBar = document.getElementById('search-bar');
 searchIcon.addEventListener('click', () => {
   searchBar.style.display = searchBar.style.display === 'none' ? 'block' : 'none';
 });
+
+const dropdownItems = document.querySelectorAll('.dropdown-item');
+const lightFittingsItem = document.getElementById('light-fit'); // Select the Light Fittings item
+const lightFittingsList = document.getElementById('drop-down');// Select the next sibling element (the dropdown list)
+dropdownItems.forEach(item => {
+  item.addEventListener('click', function() {
+    // Clear any previous selection styles
+    dropdownItems.forEach(otherItem => otherItem.classList.remove('active'));
+    
+    // Toggle active class and icon rotation for the clicked item
+    this.classList.toggle('active');
+    const icon = this.querySelector('i'); // Select the icon within the item
+    icon.classList.toggle('fa-rotate-180'); // Toggle rotation class for the icon
+
+  });
+});
+
+lightFittingsItem.addEventListener('click', () => {  
+    // Toggle visibility of the Light Fittings list
+    lightFittingsList.style.display = lightFittingsList.style.display === 'block' ? 'none' : 'block';
+});
